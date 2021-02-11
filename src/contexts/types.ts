14969c30cf6c213147943151849f9a809c8a9a59
sujanthakare <sujandt@gmail.com>
2021-02-11 11:01:08 +0530

    Fixed formatting

--- src/contexts/types.ts
+++ src/contexts/types.ts
@@ -1,51 +1,51 @@
 export enum GameType {
-	V75 = 'V75',
-	V65 = 'V65',
-	V64 = 'V64',
-	V4 = 'V4',
+  V75 = 'V75',
+  V65 = 'V65',
+  V64 = 'V64',
+  V4 = 'V4',
 }
 
 export type GameInfo = {
-	id: string;
-	startTime: string; // YYYY-MM-DDThh:mm:ss
+  id: string;
+  startTime: string; // YYYY-MM-DDThh:mm:ss
 };
 
 export interface IGameSchedule {
-	betType?: string;
-	upcoming?: Array<GameInfo>;
-	results?: Array<GameInfo>;
+  betType?: string;
+  upcoming?: Array<GameInfo>;
+  results?: Array<GameInfo>;
 }
 
 export type GameResponse = {
-	id: string;
-	races: Array<GameRace>;
-	status: string;
+  id: string;
+  races: Array<GameRace>;
+  status: string;
 };
 
 export type GameRace = {
-	date: string;
-	name: string;
-	number: number;
-	startTime: string;
-	scheduledStartTime: string;
-	starts: Array<RaceStart>;
+  date: string;
+  name: string;
+  number: number;
+  startTime: string;
+  scheduledStartTime: string;
+  starts: Array<RaceStart>;
 };
 
 export interface IPerson {
-	firstName: string;
-	lastName: string;
+  firstName: string;
+  lastName: string;
 }
 
 export type RaceStart = {
-	number: number;
-	driver: IPerson;
-	horse: {
-		name: string;
-		trainer: IPerson;
-		pedigree: {
-			father: {
-				name: string;
-			};
-		};
-	};
+  number: number;
+  driver: IPerson;
+  horse: {
+    name: string;
+    trainer: IPerson;
+    pedigree: {
+      father: {
+        name: string;
+      };
+    };
+  };
 };
--- src/lib/select.tsx
+++ src/lib/select.tsx
@@ -1,23 +1,23 @@
 import React, { HTMLProps } from 'react';
 
 interface ISelectProps {
-	options: Array<{ text: string; value: string }>;
-	placeholder: string;
+  options: Array<{ text: string; value: string }>;
+  placeholder: string;
 }
 
 const Select: React.FC<ISelectProps & HTMLProps<HTMLSelectElement>> = ({ options, placeholder, ...props }) => {
-	return (
-		<select {...props}>
-			{placeholder && <option value="">{placeholder}</option>}
-			{options.map((item, index) => {
-				return (
-					<option key={index} value={item.value}>
-						{item.text}
-					</option>
-				);
-			})}
-		</select>
-	);
+  return (
+    <select {...props}>
+      {placeholder && <option value="">{placeholder}</option>}
+      {options.map((item, index) => {
+        return (
+          <option key={index} value={item.value}>
+            {item.text}
+          </option>
+        );
+      })}
+    </select>
+  );
 };
 
 export default Select;
